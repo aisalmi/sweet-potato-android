9094b4ab3e81837ca67593f795bdee0d1c395efc
Ludovic ROLAND <ludovic.roland@hagergroup.com>
2019-09-12 15:18:48 +0200

    Update the library module on the API 29 + Add a default fragment annotation on the DummySweetFragment class

--- library/src/main/kotlin/com/hagergroup/sweetpotato/fragment/app/DummySweetFragment.kt
+++ library/src/main/kotlin/com/hagergroup/sweetpotato/fragment/app/DummySweetFragment.kt
@@ -1,6 +1,7 @@
 package com.hagergroup.sweetpotato.fragment.app
 
 import androidx.annotation.RestrictTo
+import com.hagergroup.sweetpotato.annotation.SweetFragmentAnnotation
 
 /**
  * An internal default [SweetFragment] class in order to provide a default value to the [com.hagergroup.sweetpotato.annotation.SweetActivityAnnotation] annotation.
@@ -9,6 +10,7 @@ import androidx.annotation.RestrictTo
  * @since 2018.11.07
  */
 @RestrictTo(RestrictTo.Scope.LIBRARY)
+@SweetFragmentAnnotation(layoutId = android.R.layout.list_content)
 internal class DummySweetFragment
   : SweetFragment<DummySweetFragmentAggregate>()
 {
